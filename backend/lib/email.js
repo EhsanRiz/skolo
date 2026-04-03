effd84a2ada83d200f2123e4b6aa9073fbe611a0
EhsanRiz <ehsan@4dcs.co.ls>
2026-04-03 15:02:28 +0000

    Email: use verified noreply@4dcs.co.za as FROM address

--- backend/lib/email.js
+++ backend/lib/email.js
@@ -2,7 +2,7 @@ const { Resend } = require('resend')
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-const FROM = 'Skolo <notifications@skolo.app>'
+const FROM = 'Skolo <noreply@4dcs.co.za>'
 
 /**
  * Send a waiver request email to the principal/admin
